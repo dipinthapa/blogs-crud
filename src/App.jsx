1014336b7ef6e2c797615ac28ee37a01693aa3a9
unknown <dipin.thapa1@gmail.com>
2026-04-08 16:14:08 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-blue-500">Tailwind is working</p>
+    </div>
+  )
+}
+
+export default App
